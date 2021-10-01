9cb84ec0d237622110e11a31a8c135e9c889ab05
kojin0222 <89342981+kojin0222@users.noreply.github.com>
2021-10-01 19:47:26 +0900

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer class="l-footer">
+    <h4>ショップ情報 ｜ ヒストリー</h4>
+    <p>Copyright: RaiseTech</p>
+</footer>
+</body>
+<?php wp_footer(); ?>
+
+</html>
---
+++ js/main.js
@@ -0,0 +1,11 @@
+jQuery(function(){
+
+  jQuery(".openbtn").click(function () {
+    jQuery(this).toggleClass('active');
+    jQuery(".l-sidebar").toggleClass('active');
+    jQuery(".l-sidebar-mask").toggleClass('active');
+    jQuery(".l-main-area").toggleClass('active');
+
+  });
+
+});
